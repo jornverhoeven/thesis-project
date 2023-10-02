1d2516032a83694d28d1bb4809b936e27741387e
Jorn Verhoeven <jorn.j.verhoeven@gmail.com>
2023-10-02 22:51:32 +0200

    update runExperiments script

--- runExperiments.sh
+++ runExperiments.sh
@@ -1,7 +1,8 @@
 infra=complex-infra.yml
 scenarios=(no-change risk-introduction growing unstable)
-# scenarios=(growing)
+# scenarios=(no-change)
 features=(local knowledge-sharing auctioning)
+# features=(auctioning)
 
 CURRENT=$(pwd)
 
@@ -14,3 +15,25 @@ for s in ${scenarios[@]}; do
         java -Dfile.encoding=UTF-8 -classpath /Users/jornverhoeven/dev/jornverhoeven/thesis/packages/core2/target/classes:/Users/jornverhoeven/.m2/repository/org/apache/logging/log4j/log4j-api/2.20.0/log4j-api-2.20.0.jar:/Users/jornverhoeven/.m2/repository/org/apache/logging/log4j/log4j-core/2.20.0/log4j-core-2.20.0.jar:/Users/jornverhoeven/.m2/repository/org/yaml/snakeyaml/2.0/snakeyaml-2.0.jar tech.jorn.adrian.experiment.ExperimentRunner $infra $s $f 2>&1 | tee -a $CURRENT/output/$s/$f/output.txt
     done
 done
+
+# Run mutliple times
+runs=(1 2 3 4 5)
+for s in ${runs[@]}; do
+    mkdir -p $CURRENT/output/multi-run/$s
+    mkdir -p $CURRENT/output/multi-run/$s/graphs
+    cd $CURRENT/output/multi-run/$s/
+    echo "Run $s"
+    java -Dfile.encoding=UTF-8 -classpath /Users/jornverhoeven/dev/jornverhoeven/thesis/packages/core2/target/classes:/Users/jornverhoeven/.m2/repository/org/apache/logging/log4j/log4j-api/2.20.0/log4j-api-2.20.0.jar:/Users/jornverhoeven/.m2/repository/org/apache/logging/log4j/log4j-core/2.20.0/log4j-core-2.20.0.jar:/Users/jornverhoeven/.m2/repository/org/yaml/snakeyaml/2.0/snakeyaml-2.0.jar tech.jorn.adrian.experiment.ExperimentRunner $infra no-change full 2>&1 | tee -a $CURRENT/output/multi-run/$s/output.txt
+done
+
+# Small infra
+for f in ${features[@]}; do
+    mkdir -p $CURRENT/output/small/$f
+    mkdir -p $CURRENT/output/small/$f/graphs
+    cd $CURRENT/output/small/$f
+    echo "Running experiment for small infra and feature $f"
+    java -Dfile.encoding=UTF-8 -classpath /Users/jornverhoeven/dev/jornverhoeven/thesis/packages/core2/target/classes:/Users/jornverhoeven/.m2/repository/org/apache/logging/log4j/log4j-api/2.20.0/log4j-api-2.20.0.jar:/Users/jornverhoeven/.m2/repository/org/apache/logging/log4j/log4j-core/2.20.0/log4j-core-2.20.0.jar:/Users/jornverhoeven/.m2/repository/org/yaml/snakeyaml/2.0/snakeyaml-2.0.jar tech.jorn.adrian.experiment.ExperimentRunner $infra no-change $f 2>&1 | tee -a $CURRENT/output/small/$f/output.txt
+done
+
+cd $CURRENT
+node ./packages/thesis/createGraphs.js
